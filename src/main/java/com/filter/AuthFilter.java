44f215862477e5f2b8fb8e3bbcdd01f41b43376d
8972-amick <khadkaamick2@gmail.com>
2026-04-17 08:58:38 +0545

    webfilter added

--- src/main/java/com/filter/AuthFilter.java
+++ src/main/java/com/filter/AuthFilter.java
@@ -1,12 +1,14 @@
 package com.filter;
 
 import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
+@WebFilter("/*")
 public class AuthFilter implements Filter {
 
     @Override
